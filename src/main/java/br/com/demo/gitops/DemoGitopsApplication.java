0959780eabfceafa21aac095fe1397a087c85606
Carlos FB Nepomuceno <cfbnepomuceno@gmail.com>
2026-06-28 22:20:58 -0300

    feat: aplicação spring boot inicial

---
+++ src/main/java/br/com/demo/gitops/DemoGitopsApplication.java
@@ -0,0 +1,13 @@
+package br.com.demo.gitops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoGitopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoGitopsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/demo/gitops/DemoGitopsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.demo.gitops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoGitopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
