8bc2afe59bfc02b33502201667a315792aba290b
Carlos FB Nepomuceno <cfbnepomuceno@gmail.com>
2026-06-29 09:09:30 -0300

    ci: hello demo controller and tests

---
+++ src/test/java/br/com/demo/gitops/HelloControllerTests.java
@@ -0,0 +1,31 @@
+package br.com.demo.gitops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest
+public class HelloControllerTests {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void deveRetornarSaudacaoComSucesso() throws Exception {
+        mockMvc.perform(get("/hello"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Hello from GitOps!"));
+    }
+
+    @Test
+    public void deveRetornar404ParaRotaInexistente() throws Exception {
+        mockMvc.perform(get("/rota-que-nao-existe"))
+                .andExpect(status().isNotFound());
+    }
+    
+}
